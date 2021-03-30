15d96acfca49f736938124726e9d3e2bd65789bb
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2021-03-30 21:08:14 +0200

    Initial ImageDownloader app

---
+++ ImageDownloader/ImageDownloader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageDownloader
+//
+//  Created by Dmytro Anokhin on 30/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageDownloader/ImageDownloader/ImageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloaderApp.swift
+//  ImageDownloader
+//
+//  Created by Dmytro Anokhin on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
